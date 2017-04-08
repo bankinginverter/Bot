023a77339dd4e440e047b59d3587c00989cc7f74
banking <bankinginverter@gmail.com>
2017-04-08 11:10:58 +0700

    First commit

---
+++ Assets/Bot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    public Transform player;
+    float s = 1.0f;
+    void Start ()
+    {
+
+    }
+    void Update ()
+    {
+        FollowSet();
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            s = 0f;
+            Debug.Log("Hit");
+        }
+    }
+    public void FollowSet()
+    {
+        float botpositionZ = this.transform.position.z;
+        float botpositionX = this.transform.position.x;
+        float playerpositionZ = player.transform.position.z;
+        float playerpositionX = player.transform.position.x;
+        float sumpositionZ = botpositionZ - playerpositionZ;
+        float sum = Vector3.Distance(this.transform.position, player.transform.position);
+        checkAxisZ(sum,sumpositionZ);
+        checkAxisX();
+        //this.transform.position += new Vector3(s, 0, 0)*Time.deltaTime;
+    }
+    public void checkAxisZ(float sum,float sumpositionZ)
+    {
+        //if (sum >= Mathf.Floor(1f))
+        //{
+        //    s = 1f;
+        //}
+        //if (sumpositionZ >= Mathf.Floor(0f))
+        //{
+        //    this.transform.position += new Vector3(0f, 0f, -s) * Time.deltaTime;
+        //}
+        //if (sumpositionZ <= Mathf.Floor(0f))
+        //{
+        //    this.transform.position += new Vector3(0f, 0f, s) * Time.deltaTime;
+        //}
+    }
+    public void checkAxisX()
+    {
+        Vector3 a = player.transform.position - this.transform.position;
+        
+    }
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        Contro();
+	}
+    void Contro()
+    {
+        float h = 2.0f * Input.GetAxis("Horizontal")*Time.deltaTime;
+        float v = 2.0f * Input.GetAxis("Vertical")*Time.deltaTime;
+        transform.Translate(h, 0, 0);
+        transform.Translate(0, 0, v);
+    }
+}
